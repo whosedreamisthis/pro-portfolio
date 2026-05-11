4621c3513a33893be02b31e9f63de0c0e4e66022
Dana Sharon <whosedreamisthis@gmail.com>
2026-05-11 12:28:48 -0700

    Update `ProjectCard` UI and enhance `Projects` section layout
    - Add image support with `next/image` in `ProjectCard` for improved visuals.
    - Refactor `ProjectCard` styles for better spacing and alignment.
    - Update `Button` styles for consistent design.
    - Implement responsive grid layout in `Projects` to display multiple project cards.

--- components/landing-lage/ProjectCard.tsx
+++ components/landing-lage/ProjectCard.tsx
@@ -8,17 +8,26 @@ import {
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 const ProjectCard = ({ project }) => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="font-bold">{project.title}</CardTitle>
-        <CardDescription className="text-sm text-muted-foreground">
+    <Card className="overflow-hidden flex flex-col h-full p-0 gap-4 w-[90%] mx-auto">
+      <div className="relative w-full h-48">
+        <Image
+          src={project.image}
+          fill
+          className="object-cover"
+          alt={project.title}
+        />
+      </div>
+      <CardHeader className="p-2">
+        <CardTitle className="font-bold px-2">{project.title}</CardTitle>
+        <CardDescription className="text-sm text-muted-foreground mx-2">
           {project.description}
         </CardDescription>
         <CardContent className="m-0 p-0 my-2">
-          <div className="flex gap-3 flex-row">
+          <div className="flex gap-1 flex-row my-4 mx-2">
             {project.techStack.map((tech) => (
               <Badge
                 key={tech}
@@ -28,9 +37,16 @@ const ProjectCard = ({ project }) => {
               </Badge>
             ))}
           </div>
-          <div className="flex justify-between items-center">
-            <Button>Live Demo</Button>
-            <Button>Source Code</Button>
+          <div className="flex justify-between items-center mx-auto gap-2">
+            <Button className="mx-2 rounded-md px-6  sm:px-9 ">
+              Live Demo
+            </Button>
+            <Button
+              variant="outline"
+              className="mx-2 rounded-md px-6 sm:px-9   border-brand-400 border"
+            >
+              Source Code
+            </Button>
           </div>
         </CardContent>
       </CardHeader>
--- components/landing-lage/Projects.tsx
+++ components/landing-lage/Projects.tsx
@@ -11,7 +11,11 @@ const Projects = () => {
         description="A selection of my recent work and side projects"
       />
       <div className="mb-20">
-        <ProjectCard project={projects[0]} />
+        <div className="w-[90%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto">
+          <ProjectCard project={projects[0]} />
+          <ProjectCard project={projects[0]} />
+          <ProjectCard project={projects[0]} />
+        </div>
       </div>
     </section>
   );
